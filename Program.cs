6da268e31c681d7c398753b47e2e324e6e183df6
Iliass <iliasshlm@gmail.com>
2023-12-01 16:47:48 +0100

    feat: implement face recognition

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using BlazorAI.Data;
+using BlazorAI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<FacerecognitionService>();
 builder.Services.AddHttpClient();
 
 var app = builder.Build();
